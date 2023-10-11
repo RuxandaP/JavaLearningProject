e4fa06be3e45ddd25e9d246bfe0f15d5bb40211e
Ruxanda <postolachi.ruxanda@gmail.com>
2023-10-11 16:49:20 +0300

    Added the implementation of a library system by using the collections.

---
+++ src/main/java/md/tekwillacademy/bookcollectiontask/Library.java
@@ -0,0 +1,49 @@
+package md.tekwillacademy.bookcollectiontask;
+
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Library {
+
+     private List<Book> libraryStock;
+
+     private String libraryName;
+
+     public Library(String name){
+         libraryName = name;
+         libraryStock = new ArrayList<>();
+     }
+
+     public void addBook(Book bookElement){
+         boolean operationStatus = libraryStock.add(bookElement);
+         if (operationStatus){
+             System.out.println("The book " + bookElement.getTitle() + " has been added" );
+         } else {
+             System.out.println("The book " + bookElement.getTitle() + " has not been added");
+         }
+     }
+
+     public int getTheStockStatusNumber(){
+         return libraryStock.size();
+     }
+
+     public boolean isThereSuchABookInTheStock(Book book){
+         return libraryStock.contains(book);
+     }
+
+     public void deleABookIfExists(Book book){
+         if(libraryStock.remove(book)){
+             System.out.println("The book " + book.getTitle() + "has been deleted");
+         }else {
+             System.out.println("The book " + book.getTitle() + "has not benn found");
+         }
+     }
+
+     public void printAllTheBooks(){
+
+         for(int i = 0; i< libraryStock.size(); i++){
+             libraryStock.get(i).printTheBookDetails();
+         }
+     }
+}
